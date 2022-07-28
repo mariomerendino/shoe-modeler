8137e0f5897ddf36b8af7b0c64682d405e86d982
Mario Merendino <mariomerendino@gmail.com>
2022-07-27 22:44:26 -0400

    Setup color picker

--- src/shoe/ShoeCanvas.js
+++ src/shoe/ShoeCanvas.js
@@ -1,15 +1,42 @@
-import React from 'react';
+import React, {useState} from 'react';
+import { Color } from 'three'
 import { Canvas } from "@react-three/fiber";
 import {OrbitControls, useGLTF} from '@react-three/drei'
 import NikeAirJordan from './Model';
 
 const ShoeCanvas = () => {
-  return <Canvas>
-    <ambientLight />
-    <spotLight intensity={1} angle={1} penumbra={1} position={[10, 15, 10]} />
-    <OrbitControls enablePan={true} enableZoom={true} enableRotate={true} />
-    <NikeAirJordan />
-  </Canvas>;
+  const [currentSelection, updateCurrentSelection] = useState(null)
+  const [modifiedColors, updateModifiedColors] = useState({});
+
+  const updateColor = (color) => {
+    const newColor = {}
+    newColor[currentSelection] = new Color(color);
+
+    updateModifiedColors({...modifiedColors, ...newColor})
+  }
+
+  return(
+    <div>
+      <Canvas>
+        <ambientLight />
+        <spotLight intensity={1} angle={1} penumbra={1} position={[10, 15, 10]} />
+        <OrbitControls enablePan={true} enableZoom={true} enableRotate={true} />
+        <NikeAirJordan
+          currentSelection={currentSelection}
+          updateCurrentSelection={updateCurrentSelection}
+          modifiedColors={modifiedColors}
+        />
+      </Canvas>
+
+      {currentSelection && 
+        <div>
+          {currentSelection}
+          <input type="color" onChange={e => updateColor(e.target.value)} />
+        </div>
+      }
+    </div>
+  );
+
 };
 
 export default ShoeCanvas;
