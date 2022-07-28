ccc82d3d7fd4c454d8b0c9fc7619c0ac17508d97
Mario Merendino <mariomerendino@gmail.com>
2022-07-27 23:14:19 -0400

    css cleanup

--- src/shoe/ShoeCanvas.js
+++ src/shoe/ShoeCanvas.js
@@ -16,7 +16,7 @@ const ShoeCanvas = () => {
   }
 
   return(
-    <div>
+    <div className={'mainCanvasArea'}>
       <Canvas>
         <ambientLight />
         <spotLight intensity={1} angle={1} penumbra={1} position={[10, 15, 10]} />
