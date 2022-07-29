6d186b37e037fab8d89efc84a5fc818f2c496547
Mario Merendino <mariomerendino@gmail.com>
2022-07-28 23:27:24 -0400

    add loader

--- src/shoe/ShoeCanvas.js
+++ src/shoe/ShoeCanvas.js
@@ -1,8 +1,9 @@
-import React, {useState} from 'react';
+import React, {Suspense, useState} from 'react';
 import { Color } from 'three'
 import { Canvas } from "@react-three/fiber";
 import {OrbitControls} from '@react-three/drei'
 import NikeAirJordan from './NikeAirJordan/Model';
+import Loader from '../components/Loader';
 
 const ShoeCanvas = () => {
   const [currentSelection, updateCurrentSelection] = useState(null)
@@ -17,17 +18,20 @@ const ShoeCanvas = () => {
 
   return(
     <div className={'mainCanvasArea'}>
-      <Canvas>
-        <ambientLight intensity={.25}/>
-        <spotLight intensity={1} angle={1} penumbra={1} position={[0, 0, 180]} />
-        <OrbitControls enablePan={true} enableZoom={true} enableRotate={true} />
-        <NikeAirJordan
-          currentSelection={currentSelection}
-          updateCurrentSelection={updateCurrentSelection}
-          modifiedColors={modifiedColors}
-        />
-        <spotLight intensity={1} angle={10} penumbra={1} position={[180, 180, 180]} />
-      </Canvas>
+      <Suspense fallback={<Loader />}>
+        <Canvas>
+          <ambientLight intensity={.25}/>
+          <spotLight intensity={1} angle={1} penumbra={1} position={[0, 0, 180]} />
+          <OrbitControls enablePan={true} enableZoom={true} enableRotate={true} />
+          <NikeAirJordan
+            currentSelection={currentSelection}
+            updateCurrentSelection={updateCurrentSelection}
+            modifiedColors={modifiedColors}
+          />
+          <spotLight intensity={1} angle={10} penumbra={1} position={[180, 180, 180]} />
+        </Canvas>
+      </Suspense>
+
 
       {currentSelection && 
         <div>
